c2d45b2d5a378ed3ffed55357c166178e1129bcb
rettori <rettori@gmail.com>
2014-07-11 02:23:45 -0300

    AWDWA

--- src/main/java/com/redhat/netty/NettyServerHandler.java
+++ src/main/java/com/redhat/netty/NettyServerHandler.java
@@ -1,14 +1,23 @@
 package com.redhat.netty;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
 
 public class NettyServerHandler extends ChannelInboundHandlerAdapter { // (1)
 
 	 @Override
 	    public void channelRead(ChannelHandlerContext ctx, Object msg) {
-	        ctx.write(msg); // (1)
-	    	ctx.flush(); // (2)
+		    ByteBuf in = (ByteBuf) msg;
+		    try {
+		        while (in.isReadable()) { // (1)
+		            System.out.print((char) in.readByte());
+		            System.out.flush();
+		        }
+		    } finally {
+		        ReferenceCountUtil.release(msg); // (2)
+		    }
 	    }
 	 
 	@Override
